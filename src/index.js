c6f355f2233a735e976e4254fd00f7399eb06295
kattrinkr <kattrinkr@mail.ru>
2018-02-22 10:31:41 +0300

    The third

--- src/index.js
+++ src/index.js
@@ -1,30 +1,54 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.sortermas = [];
+    this.CompareFunc = function(a,b) {return a-b};
+// Создаём экземпляр класса Sorter
   }
 
   add(element) {
-    // your implementation
+    this.sortermas.push(element);// Добавление в массив
   }
 
   at(index) {
-    // your implementation
+    return this.sortermas[index];// Вывод одного элемента по индексу
   }
 
   get length() {
-    // your implementation
+    return this.sortermas.length;// Длина массива
   }
 
   toArray() {
-    // your implementation
+    return this.sortermas;// Вывод всех элементов массива
   }
 
   sort(indices) {
-    // your implementation
+    var sortmas = [];
+    var index = [];
+    for (var k = 0; k < indices.length; k++)
+      index.push(indices[k]);//Массив Index заполнится индексами, которые нужно сортировать
+
+    for (var k = 0; k < indices.length; k++)
+      sortmas.push(this.sortermas[indices[k]]);//Массив Sortmas заполнится значениями исх. массива по индексам
+
+    for (var j = 0; j < indices.length-1; j++)
+      for (var i = 0; i < (indices.length-1-j); i++)
+        if (sortmas[j] > sortmas[j+1]){
+          var a = sortmas[i];
+          sortmas[i] = sortmas[i+1];
+          sortmas[i+1] = a;
+        }// Сортировка элементов массива
+
+    for (var i = 0; i <= this.sortermas.length; i++)//Меняю отсортированные значения в исходном массиве
+      for (var k = 0; k <= index.length; k++){
+        if (i === index[k]){
+          this.sortermas[i] = sortmas[k];
+        }
+      }
+
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.CompareFunc = compareFunction;//Сравнение по свойству или строкам
   }
 }
 
